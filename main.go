bd2d6d07fc9d5372168e10002035438fc228f341
kekenalog <ywang3394@gmail.com>
2023-01-16 00:30:09 +0800

    init: Go struct

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	// 创建一个默认的路由引擎
+	r := gin.New()
+
+	// 配置路由
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{ // c.JSON: 返回 JSON 格式的数据
+			"message": "Hello world! I from Gin.",
+		})
+	})
+
+	// 启动 HTTP 服务，监听 8080 端口
+	r.Run(":8080")
+
+}
